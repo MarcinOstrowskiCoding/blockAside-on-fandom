18cde0cfe742d234f77e36cd124d82e569580d4d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-07-22 21:17:05 +0200

    Creating repo for Mozilla extension: Block Aside on Fandom Wiki.

---
+++ blockAside.js
@@ -0,0 +1,3 @@
+const asideCollection = document.getElementsByClassName('page__right-rail')
+const blockAsideElement = asideCollection.item(0)
+if (blockAsideElement) blockAsideElement.style.display = 'none'
